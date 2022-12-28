f0d1f41f2afc874b3278b2080240ef01a21d9c14
Andrey Potapov <andrey_potapov@rntgroup.com>
2022-12-28 22:42:53 +0400

    query for testing words.fun_language_create is ready

--- scripts/poc/poc proc_language_create.sql
+++ scripts/poc/poc proc_language_create.sql
@@ -7,4 +7,6 @@
 
 
 
-select words.proc_language_create('[]')
+select words.fun_language_create('{"language_id":0,"language_name":"греческий","language_shortname":"гр1","created_at":null,"changed_at":null}','{}','{}')
+
+{"language_id" : 41, "language_name" : "греческий", "language_shortname" : "гр", "created_at" : "2022-12-28T22:38:00.639217", "changed_at" : "2022-12-28T22:38:00.639217"}
